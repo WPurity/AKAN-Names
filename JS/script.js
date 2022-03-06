9db9e357d8bad95494acb2ab768b1bc0dc1f4b24
WPurity <waititu2015@gmail.com>
2022-03-06 19:59:33 +0300

    add event listener

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,8 @@ const inputGender = document.querySelectorAll("#identity");
 const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 const namesOfMales = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const namesOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+entryData.addEventListener("click", function (stopRepeat) {
+    stopRepeat.preventDefault();
+
+    })
+    
