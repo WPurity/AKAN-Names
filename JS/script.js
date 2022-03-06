a9077fcb3c59855f985ecfac726a8556369cc3c9
WPurity <waititu2015@gmail.com>
2022-03-06 19:57:01 +0300

    Added arrays

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,6 @@
 const entryData = document.getElementById("submit");
 const giveName = document.querySelector("#display");
+const inputGender = document.querySelectorAll("#identity");
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const namesOfMales = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const namesOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
